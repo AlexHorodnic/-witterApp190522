7d33f44ce2aae03d1362c9e0bded1f07b9821565
alexandru horodnic <ahorodnic@nyc.gr>
2022-05-19 19:14:25 +0300

    added a filtered stream with one filter "java"

--- ΤwitterApp190522/Program.cs
+++ ΤwitterApp190522/Program.cs
@@ -6,3 +6,16 @@ var userClient = new TwitterClient(userCredentials);
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+// Create a simple stream containing only tweets with the keyword France
+
+var stream = userClient.Streams.CreateFilteredStream();
+stream.AddTrack("java");
+
+stream.MatchingTweetReceived += (sender, eventReceived) =>
+{
+    Console.WriteLine(eventReceived.Tweet);
+};
+
+await stream.StartMatchingAnyConditionAsync();
