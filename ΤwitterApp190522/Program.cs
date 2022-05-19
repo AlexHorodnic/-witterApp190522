5ebbde8e18f4fbc986a8252cf17cc5f937ea7eaf
KonstantinosMr <100868977+KonstantinosMr@users.noreply.github.com>
2022-05-19 20:30:24 +0300

    FIXED COUNTER 4

--- ΤwitterApp190522/Program.cs
+++ ΤwitterApp190522/Program.cs
@@ -21,7 +21,7 @@ stream.AddTrack("javascript");
 int counter1 = 0;
 int counter2 = 0;
 int counter3 = 0;
-int counter 4 = 0;
+int counter4 = 0;
 
 stream.MatchingTweetReceived += (sender, eventReceived) =>
 {
