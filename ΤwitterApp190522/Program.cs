c7a5cc2be7fa891847fa27e285a85d16baabc674
KonstantinosMr <100868977+KonstantinosMr@users.noreply.github.com>
2022-05-19 20:45:31 +0300

    Added counters

--- ΤwitterApp190522/Program.cs
+++ ΤwitterApp190522/Program.cs
@@ -1,31 +1,96 @@
-﻿using Tweetinvi;
-using Tweetinvi.Models;
-using System;
+﻿using System;
 using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Models;
 
-var userCredentials = new TwitterCredentials("nN2yYQcMWlhsvaa8awvHo9PWF", "MDGDCAAhOaA3BSVMSz5HF2LElow39rdJlPJxdQO9z1E4pZ03b3", "1629692186-BimLBhF2MUQZ5ufDZ4G93ufBy7byABzijbZcNqQ", "hdY2py8hMvdSb7tU0a0wDxLmkKZrH7Gj24P7XL0SpAjTo");
-var userClient = new TwitterClient(userCredentials);
-
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-// Create a simple stream containing only tweets with the keyword France
-
-var stream = userClient.Streams.CreateFilteredStream();
-stream.AddTrack("java");
-stream.AddTrack("python");
-stream.AddTrack("csharp");
-stream.AddTrack("javascript");
-
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-
-stream.MatchingTweetReceived += (sender, eventReceived) =>
+namespace Program
 {
-    Console.WriteLine(eventReceived.Tweet);
-};
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
 
-await stream.StartMatchingAnyConditionAsync();
+
+
+            string filter1 = "javascript";
+            string filter2 = "python";
+            string filter3 = "c#";
+            string filter4 = "java";
+
+            var userCredentials = new TwitterCredentials("nN2yYQcMWlhsvaa8awvHo9PWF", "MDGDCAAhOaA3BSVMSz5HF2LElow39rdJlPJxdQO9z1E4pZ03b3", "1629692186-BimLBhF2MUQZ5ufDZ4G93ufBy7byABzijbZcNqQ", "hdY2py8hMvdSb7tU0a0wDxLmkKZrH7Gj24P7XL0SpAjTo");
+            var userClient = new TwitterClient(userCredentials);
+
+            // Create a simple stream containing only tweets with the keyword France
+
+            var stream = userClient.Streams.CreateFilteredStream();
+            stream.AddTrack(filter1);
+            stream.AddTrack(filter2);
+            stream.AddTrack(filter3);
+            stream.AddTrack(filter4);
+
+            int counter1 = 0;
+            int counter2 = 0;
+            int counter3 = 0;
+            int counter4 = 0;
+
+            var howManyTweetsSoFar = 0;
+
+            stream.MatchingTweetReceived += (sender, eventReceived) =>
+            {
+
+                Console.WriteLine(eventReceived.Tweet);
+
+                ITweet tweet = eventReceived.Tweet;
+                string tweetText = tweet.FullText;
+                int favoriteCount = tweet.FavoriteCount;
+                string tweetAuthorName = tweet.CreatedBy.Name;
+
+
+                Console.WriteLine();
+                Console.WriteLine(tweetAuthorName + " " + favoriteCount);
+                Console.WriteLine(tweetText);
+
+                if (tweetText.Contains(filter1))
+                {
+                    counter1++;
+                }
+                if (tweetText.Contains(filter2))
+                {
+                    counter2++;
+                }
+
+                if (tweetText.Contains(filter3))
+                {
+                    counter3++;
+                }
+
+                if (tweetText.Contains(filter4))
+                {
+                    counter4++;
+                }
+
+
+
+
+                Console.WriteLine(
+                    filter1 + ":" + counter1 + ", " +
+                    filter2 + ":" + counter2 + ", " +
+                    filter3 + ":" + counter3 + ", " +
+                    filter4 + ":" + counter4
+                    );
+                Console.WriteLine("-------------------------");
+
+                if (howManyTweetsSoFar == 10)
+                {
+                    stream.Stop();
+                    Console.WriteLine("Complete!");
+                }
+            };
+
+            await stream.StartMatchingAnyConditionAsync();
+
+
+        }
+
+    }
+}
