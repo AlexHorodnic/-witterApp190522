94fbfbb36bd005df14577dd671a72ba2397f4057
alexandru horodnic <ahorodnic@nyc.gr>
2022-05-19 19:06:41 +0300

    added library and credentials

--- ΤwitterApp190522/Program.cs
+++ ΤwitterApp190522/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Tweetinvi;
+using Tweetinvi.Models;
+
+var userCredentials = new TwitterCredentials("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
+var userClient = new TwitterClient(userCredentials);
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
